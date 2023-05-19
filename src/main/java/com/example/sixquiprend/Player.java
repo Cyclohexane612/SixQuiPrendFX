18b62ea62b6087b9b4c79d50cab95d92785ec550
aymaneh22 <aymaneh.ah@gmail.com>
2023-05-19 15:20:06 +0200

    class player

--- src/main/java/com/example/sixquiprend/Player.java
+++ src/main/java/com/example/sixquiprend/Player.java
@@ -1,7 +1,29 @@
 package com.example.sixquiprend;
 
 import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player {
+    @Getter @Setter
+    private String name;
+    @Getter @Setter
+    private int numberTdb;
+    @Getter @Setter
+    private List<Card> hand;
+    public Player(String name){
+        this.name = name;
+        this.numberTdb = 0;
+        this.hand = new ArrayList<Card>();
+    }
+    public int Tdb(List<Card> listOfCard){
+        int numberOfTdb = 0;
+        for(int i = 0; i < listOfCard.size(); i++){
+            numberOfTdb = numberOfTdb + listOfCard.get(i).getTdb();
+        }
+        return numberOfTdb;
+    }
 
 }
