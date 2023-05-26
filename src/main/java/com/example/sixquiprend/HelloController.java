55ca6d5fba53af5a67aebae62c235baf7fa790c5
Cyclohexane612 <hluc2002@gmail.com>
2023-05-26 15:22:03 +0200

    Demander le nom

--- src/main/java/com/example/sixquiprend/HelloController.java
+++ src/main/java/com/example/sixquiprend/HelloController.java
@@ -1,10 +1,15 @@
 package com.example.sixquiprend;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class HelloController {
+    @FXML
+    private VBox name;
     @FXML
     private Label welcomeText;
     @FXML
@@ -12,4 +17,7 @@ public class HelloController {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 
+    public void confirmName(ActionEvent actionEvent) {
+        name.setVisible(false);
+    }
 }
