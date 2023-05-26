a2cc60fa90ecd1cadddbc293c1a5c46820fe1632
Cyclohexane612 <hluc2002@gmail.com>
2023-05-26 17:01:44 +0200

    Première scène fini

---
+++ src/main/java/com/example/sixquiprend/Board.java
@@ -0,0 +1,13 @@
+package com.example.sixquiprend;
+
+import java.util.List;
+
+public class Board {
+    private List<Card> row1, row2, row3, row4;
+    public void Board(){
+        this.row1 = row1;
+        this.row2 = row2;
+        this.row3 = row3;
+        this.row4 = row4;
+    }
+}
