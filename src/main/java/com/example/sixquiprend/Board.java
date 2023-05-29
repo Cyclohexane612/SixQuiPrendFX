9134bf361dcc6272f2aabae591fbc0e18fe68b7a
Maksu258 <maxence@greus.fr>
2023-05-29 17:25:08 +0200

    Changes by ayman

--- src/main/java/com/example/sixquiprend/Board.java
+++ src/main/java/com/example/sixquiprend/Board.java
@@ -5,13 +5,21 @@ import lombok.Getter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Board {
+public class    Board {
+    @Getter
+    private List<AbstractPlayer> players;
+
     @Getter
     private List<Card> row1, row2, row3, row4;
-    public void Board(){
+
+    private Boolean endTurn;
+
+    public void Board() {
+        this.players = new ArrayList<AbstractPlayer>();
         this.row1 = new ArrayList<Card>();
         this.row2 = new ArrayList<Card>();
         this.row3 = new ArrayList<Card>();
         this.row4 = new ArrayList<Card>();
+        this.endTurn = endTurn;
     }
 }
