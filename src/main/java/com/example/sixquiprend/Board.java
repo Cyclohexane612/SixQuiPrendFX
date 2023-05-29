064b7a0c47401da4201e4711cd79b0a1d17f4ce4
Cyclohexane612 <hluc2002@gmail.com>
2023-05-29 15:54:03 +0200

    Première scène fini

--- src/main/java/com/example/sixquiprend/Board.java
+++ src/main/java/com/example/sixquiprend/Board.java
@@ -1,13 +1,17 @@
 package com.example.sixquiprend;
 
+import lombok.Getter;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
+    @Getter
     private List<Card> row1, row2, row3, row4;
     public void Board(){
-        this.row1 = row1;
-        this.row2 = row2;
-        this.row3 = row3;
-        this.row4 = row4;
+        this.row1 = new ArrayList<Card>();
+        this.row2 = new ArrayList<Card>();
+        this.row3 = new ArrayList<Card>();
+        this.row4 = new ArrayList<Card>();
     }
 }
--- src/main/java/com/example/sixquiprend/Card.java
+++ src/main/java/com/example/sixquiprend/Card.java
@@ -1,28 +1,48 @@
 package com.example.sixquiprend;
 
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import lombok.Getter;
 
 public class Card {
+    @FXML
+    @Getter
+    private Pane image;
     @Getter
     private final int number;
     @Getter
     private int tdb;
     public Card(int number){
-        this.number=number;
+        this.number = number;
+        this.tdb = getTdb();
+
+        // Créez les labels et configurez-les
+        numberLabel = new Label(String.valueOf(number));
+        tdbLabel = new Label(String.valueOf(tdb));
+
+        // Ajoutez les labels à l'image StackPane
+        image.getChildren().addAll(tdbLabel, numberLabel);
+
+        // Positionnez les labels correctement
+        StackPane.setAlignment(numberLabel, Pos.CENTER);
+        StackPane.setAlignment(tdbLabel, Pos.TOP_CENTER);
     }
+
     public int getTdb(){
-            //Define the number of beef's head
-            if(number%11 == 0 ){
-                if(number == 55 ){
-                    return 7;
-                }
-                return 5;
-            }else if(number%10 == 0){
-                return 3;
-            }else if(number%5 == 0){
-                return 2;
-            }else{
-                return 1;
+        if(number%11 == 0 ){
+            if(number == 55 ){
+                return 7;
             }
+            return 5;
+        }else if(number%10 == 0){
+            return 3;
+        }else if(number%5 == 0){
+            return 2;
+        }else{
+            return 1;
+        }
     }
 }
